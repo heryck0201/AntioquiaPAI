0678f95f6ad1d5b7b2a0f71da20738282bd76ad0
heryck0201 <herycktarso@outlook.com>
2025-01-25 17:42:35 -0300

    Adicionar arquivos de projeto.

---
+++ AntioquiaPAI/Context/AntioquiaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AntioquiaPAI.Context
+{
+    public class AntioquiaDbContext : DbContext
+    {
+        public AntioquiaDbContext(DbContextOptions<AntioquiaDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<User> User { get; set; }
+    }
+}
